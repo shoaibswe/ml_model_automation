3847253123a83c4c005fdce45f287199a8d00be4
shoaibswe <shoaibswe@gmail.com>
2023-03-30 01:08:56 +0600

    added precision and recall

--- evaluation.py
+++ evaluation.py
@@ -1,19 +1,14 @@
-from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
 
 def evaluate(y_test, y_pred, model_name):
-    # Calculate evaluation metrics
     accuracy = accuracy_score(y_test, y_pred)
-    f1 = f1_score(y_test, y_pred, average='weighted', zero_division=0)
-    
-    # Check if the problem is binary or multi-class
-    if len(set(y_test)) == 2:
-        auc = roc_auc_score(y_test, y_pred)
-    else:
-        auc = 'N/A'
+    precision = precision_score(y_test, y_pred, average="weighted")
+    recall = recall_score(y_test, y_pred, average="weighted")
+    f1 = f1_score(y_test, y_pred, average="weighted")
 
-    return {
-        "model": model_name,
-        "accuracy": accuracy,
-        "f1_score": f1,
-        "auc": auc,
-    }
+    try:
+        auc = roc_auc_score(y_test, y_proba, multi_class="ovr")
+    except NameError:
+        auc = "N/A"
+    
+    return {"model_name": model_name, "accuracy": accuracy, "precision": precision, "recall": recall, "f1_score": f1, "auc_score": auc}
