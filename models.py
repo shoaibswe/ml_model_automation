85e89955357a3ee16dc6a4ffb3d615630045457e
shoaibswe <shoaibswe@gmail.com>
2023-03-30 01:40:26 +0600

    multiple models added and multiple input files

--- models.py
+++ models.py
@@ -1,39 +1,31 @@
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.svm import SVC
 
 def train_model(train_data, model_name, target_column):
-    try:
-        X_train = train_data.drop(columns=[target_column])
-        y_train = train_data[target_column]
+    if model_name == "Logistic Regression":
+        model = LogisticRegression(random_state=42)
+    elif model_name == "Decision Tree Classifier":
+        model = DecisionTreeClassifier(random_state=42)
+    elif model_name == "Random Forest Classifier":
+        model = RandomForestClassifier(random_state=42)
+    elif model_name == "Gradient Boosting Classifier":
+        model = GradientBoostingClassifier(random_state=42)
+    elif model_name == "Support Vector Machine Classifier":
+        model = SVC(random_state=42)
 
-        if model_name == "LogisticRegression":
-            model = LogisticRegression(max_iter=1000)
-            model.fit(X_train, y_train)
-        elif model_name == "RandomForestClassifier":
-            model = RandomForestClassifier()
-            model.fit(X_train, y_train)
-        elif model_name == "SVM":
-            model = SVC(probability=True)
-            model.fit(X_train, y_train)
-        else:
-            raise ValueError("Invalid model name")
-
-        return model
-
-    except Exception as e:
-        print("Error training model:", e)
-        return "An error occurred while training the model. Please check if the training data and model name are correctly specified and try again."
+    X_train = train_data.drop(target_column, axis=1)
+    y_train = train_data[target_column]
+    model.fit(X_train, y_train)
+    return model
 
 def test_model(test_data, model, target_column):
+    X_test = test_data.drop(target_column, axis=1)
+    y_test = test_data[target_column]
+    y_pred = model.predict(X_test)
     try:
-        X_test = test_data.drop(columns=[target_column])
-        y_test = test_data[target_column]
-        y_pred = model.predict(X_test)
         y_proba = model.predict_proba(X_test)
-
-        return y_test, y_pred, y_proba
-
-    except Exception as e:
-        print("Error testing model:", e)
-        return "An error occurred while testing the model. Please check if the testing data and model are correctly specified and try again."
+    except AttributeError:
+        y_proba = None
+    return y_test, y_pred, y_proba
